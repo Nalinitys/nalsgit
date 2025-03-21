ff5c7b179128f5767bf4051f6714c054ec37c17f
nalinitys <nalt@publicisgroupe.net>
2025-03-21 12:12:24 +0530

    Updated testng and added new java program

---
+++ src/test/java/tests/simpleClass.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class simpleClass {
+	@Test
+	  public static void main(String[] args) {
+	    	 
+	    	 String str="Nalini";
+	    	System.out.print(str);
+	}
+	}
+
